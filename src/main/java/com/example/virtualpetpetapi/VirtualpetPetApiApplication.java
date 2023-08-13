c5aff4b301fed77e814c6f7a4b3fd79a8a02cbb0
Alex Ramirez <alexramirez7297@gmail.com>
2023-08-13 15:56:14 -0400

    first commit

---
+++ src/main/java/com/example/virtualpetpetapi/VirtualpetPetApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.virtualpetpetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualpetPetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualpetPetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/virtualpetpetapi/VirtualpetPetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.virtualpetpetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualpetPetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
